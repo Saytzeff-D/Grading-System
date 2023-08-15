29db0c29f85ad79f260eafc86352c86a094a11f2
David Atanda <ololadedavid15@gmail.com>
2023-08-15 10:32:24 +0100

    a simple calculator performing addition

---
+++ calc.py
@@ -0,0 +1,8 @@
+score_1 = input("Enter a number: ")
+score_2 = input("Enter another number: ")
+
+def calc(): 
+    result = float(score_1) + float(score_2)    
+    print(f"Your addition is {result}")       
+
+calc()
